a873cb5a86b2b8143d448514effa23942e75ff9d
Jan Käberich <j.kaeberich@gmx.de>
2022-08-31 00:11:55 +0200

    Added lowlevel USB echo example

---
+++ Software/LibreCAL/src/USB/usb.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef void(*usbd_recv_callback_t)(const uint8_t *buf, uint16_t len);
+
+void usb_init(usbd_recv_callback_t receive_callback);
+bool usb_transmit(const uint8_t *data, uint16_t length);
+uint16_t usb_available_buffer();
+void usb_log(const char *log, uint16_t length);
+void usb_clear_buffer();
+
+#ifdef __cplusplus
+}
+#endif
--- Software/LibreCAL/src/main.c
+++ Software/LibreCAL/src/main.c
@@ -3,6 +3,8 @@
 #include "FreeRTOS.h"
 #include "task.h"
 
+#include "USB/usb.h"
+
 
 #define LED_PIN   (25)
 
@@ -10,9 +12,16 @@ static void defaultTask(void* ptr) {
 	while(true);
 }
 
+static void usb_rx(const uint8_t *buf, uint16_t len) {
+	usb_transmit(buf, len);
+}
+
 int main(void) {
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+    gpio_put(LED_PIN, 1);
+
+    usb_init(usb_rx);
 
     xTaskCreate(defaultTask, "defaultTask", 1024, NULL, 3, NULL);
 
