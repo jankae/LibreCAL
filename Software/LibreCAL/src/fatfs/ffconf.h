26056fa1524498a2153a57381eb5ff0c24267101
bvernoux <bvernoux@gmail.com>
2023-03-22 18:17:12 +0100

    LibreCAL firmware add SCPI command :DATE_TIME to get/set LibreCAL DATE+TIME+UTC and take that into account in fatfs Change also fatfs configuration - FF_CODE_PAGE to 437 (U.S.) instead of 932 (Japanese (DBCS)) - FF_FS_EXFAT set to 0 (as we are not using EXFAT but only FAT)

--- Software/LibreCAL/src/fatfs/ffconf.h
+++ Software/LibreCAL/src/fatfs/ffconf.h
@@ -84,7 +84,7 @@
 / Locale and Namespace Configurations
 /---------------------------------------------------------------------------*/
 
-#define FF_CODE_PAGE	932
+#define FF_CODE_PAGE	437
 /* This option specifies the OEM code page to be used on the target system.
 /  Incorrect code page setting can cause a file open failure.
 /
@@ -231,13 +231,13 @@
 /  buffer in the filesystem object (FATFS) is used for the file data transfer. */
 
 
-#define FF_FS_EXFAT		1
+#define FF_FS_EXFAT		0
 /* This option switches support for exFAT filesystem. (0:Disable or 1:Enable)
 /  To enable exFAT, also LFN needs to be enabled. (FF_USE_LFN >= 1)
 /  Note that enabling exFAT discards ANSI C (C89) compatibility. */
 
 
-#define FF_FS_NORTC		1
+#define FF_FS_NORTC		0
 #define FF_NORTC_MON	1
 #define FF_NORTC_MDAY	1
 #define FF_NORTC_YEAR	2020
