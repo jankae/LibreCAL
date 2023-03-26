76e8d7731d3bc3a7cdfdef8dcf280d9b0e75039a
bvernoux <bvernoux@gmail.com>
2023-03-26 18:54:16 +0200

    Revert "fatfs/flashdisk.h adjust the size for DISK0 & DISK1 to be same size (flash.size() / 2)"
    This reverts commit 4d2030802e5793285a4a480323c19f61b96d7c86.

--- Software/LibreCAL/src/fatfs/flashdisk.h
+++ Software/LibreCAL/src/fatfs/flashdisk.h
@@ -8,8 +8,8 @@ extern Flash flash;
 
 #define FF_FLASH_DISKS			2
 // DISK0 is writable
-#define FF_FLASH_DISK1_SIZE		(flash.size() / 2)
-#define FF_FLASH_DISK0_SIZE		(flash.size() / 2)
+#define FF_FLASH_DISK1_SIZE		1048576
+#define FF_FLASH_DISK0_SIZE		(flash.size() - FF_FLASH_DISK1_SIZE)
 
 #define FF_FLASH_DISK0_SECTORS	(FF_FLASH_DISK0_SIZE / Flash::SectorSize)
 #define FF_FLASH_DISK1_SECTORS	(FF_FLASH_DISK1_SIZE / Flash::SectorSize)
